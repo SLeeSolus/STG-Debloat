2602312c1460a1db8714ea1f880d7377e20fc45c
Sean Lee <slee@solus.net>
2026-09-15 11:41:14 -0500

    Renamed files/Folders, added To-Do

--- Debloat-HPv2.ps1
+++ Debloat-HPv2.ps1
@@ -432,11 +432,11 @@ function Remove-OfficeFromRegistryFallback {
         else {
             try {
                 [xml]$configXml = @"
-<Configuration Product="$productID">
-    <Display Level="none" CompletionNotice="no" SuppressModal="yes" AcceptEula="yes" />
-    <Setting Id="SETUP_REBOOT" Value="Never" />
-</Configuration>
-"@
+			<Configuration Product="$productID">
+    			<Display Level="none" CompletionNotice="no" SuppressModal="yes" AcceptEula="yes" />
+    			<Setting Id="SETUP_REBOOT" Value="Never" />
+			</Configuration>
+			"@
                 $configXml.Save($xmlFile)
                 $UninstallString = "$($_.UninstallString) /config `"$xmlFile`""
             } catch {
